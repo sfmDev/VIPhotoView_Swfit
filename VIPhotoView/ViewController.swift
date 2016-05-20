e35adb75b3f338cd0eba032aa5e21526162f2e7c
石峰铭 <sfm.shi@5253w.com>
2016-05-20 18:57:05 +0800

    Initial Commit

---
+++ VIPhotoView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VIPhotoView
+//
+//  Created by 石峰铭 on 16/5/20.
+//  Copyright © 2016年 shifengming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
