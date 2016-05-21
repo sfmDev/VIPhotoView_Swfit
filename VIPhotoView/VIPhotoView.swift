5f76fbb05b6472f47e96829104846129fd03dcb1
石峰铭 <sfm.shi@5253w.com>
2016-05-21 15:46:27 +0800

    first commit

---
+++ VIPhotoView/VIPhotoView.swift
@@ -0,0 +1,21 @@
+//
+//  VIPhotoView.swift
+//  VIPhotoView
+//
+//  Created by 石峰铭 on 16/5/20.
+//  Copyright © 2016年 shifengming. All rights reserved.
+//
+
+import UIKit
+
+class VIPhotoView: UIScrollView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
